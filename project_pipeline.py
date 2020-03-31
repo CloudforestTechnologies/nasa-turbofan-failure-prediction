d90470c42999fd115b8d43819e6159ad49bb0fcf
PMetcalf <paulmetcalf@hotmail.com>
2020-03-31 11:28:30 +0100

    Added title to project pipeline.

---
+++ project_pipeline.py
@@ -0,0 +1,10 @@
+'''
+
+NASA Turbofan Failure Prediction
+
+This is the main script of the project.
+
+This script trains and evaluates a series of ML models to predict failure on turbofan engines using data supplied by NASA.  
+
+'''
+
