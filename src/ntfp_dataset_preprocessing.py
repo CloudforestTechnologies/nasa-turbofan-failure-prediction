502100da7f01c173274eca0b7b551a052c664a94
PMetcalf <paulmetcalf@hotmail.com>
2020-04-08 11:24:55 +0100

    Added docstring comment to preprocessing routine.

--- src/ntfp_dataset_preprocessing.py
+++ src/ntfp_dataset_preprocessing.py
@@ -9,6 +9,17 @@ This file supports performing preprocessing operations on datasets.
 # Module Importations (A - Z Format)
 ###################################
 
-#  Routine removes columns from dataset
 def dataset_remove_columns(dataset, columns_to_be_removed):
+    """
+    Removes selected columns from dataframe, returns a new data frame.
+    ======================================
+
+    Input:
+	    dataset (dataframe) - Initial dataframe, from which columns are to be removed.
+        columns_to_be_removed (list) - List of column names to be removed from dataframe.
+
+    Output:
+	    processed_dataset (dataframe) - Final dataframe, with specified columns removed.
+    """
+
     pass
