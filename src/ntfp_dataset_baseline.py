a5530c28eb483e49f506fe257f4725007b38fb21
PMetcalf <paulmetcalf@hotmail.com>
2020-04-23 11:33:04 +0100

    Added module imports to baseline.

--- src/ntfp_dataset_baseline.py
+++ src/ntfp_dataset_baseline.py
@@ -8,7 +8,11 @@ This file supports creating a baseline predictive model.
 ###################################
 # Module Importations (A - Z Format)
 ###################################
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import classification_report
+import numpy as np
 
 # Routine creates baseline model from data.
 def create_baseline_model(dataset):
