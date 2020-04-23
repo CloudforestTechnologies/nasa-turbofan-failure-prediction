ee2dfed715d3c97219ff961059f2af71b2f0b784
PMetcalf <paulmetcalf@hotmail.com>
2020-04-23 11:40:03 +0100

    Added outline to creation of baseline model.

--- src/ntfp_dataset_baseline.py
+++ src/ntfp_dataset_baseline.py
@@ -14,12 +14,30 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import classification_report
 import numpy as np
 
-# Routine creates baseline model from data.
-def create_baseline_model(dataset):
+def create_baseline_model(dataset_df):
+    """
+    Creates, trains and returns a linear regression model from training data.
+    ======================================
+
+    Input:
+        dataset_df (dataframe) - Dataframe containing training dataset.
+        
+    Output:
+	    regr_model (LinearRegression) - Linear Regression model fitted to training data.
+    """
     pass
 
-
-
-# Routine evaluates baseline model. 
 def evaluate_baseline_model(model):
+    """
+    Orders, prints and returns a list of data values sorted by absolute value.
+    ======================================
+
+    Input:
+        slopes_array (array) - Array containing normalised column data.
+        dataset_df (dataframe) - Dataframe containing parent dataset.
+        
+    Output:
+	    slope_order (array) - An array of data columns, ordered by abs value (highest first).
+    """
+
     pass
