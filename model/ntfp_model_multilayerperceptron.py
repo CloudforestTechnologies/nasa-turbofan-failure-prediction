2516cee253eab0a1f24dee99c549977329a4f19b
PMetcalf <paulmetcalf@hotmail.com>
2020-04-28 10:50:00 +0100

    Added multilayerperceptron module to project.

---
+++ model/ntfp_model_multilayerperceptron.py
@@ -0,0 +1,10 @@
+'''
+NASA Turbofan Failure Prediction - Multi-Layer Peceptron
+
+This file supports training and evaluating a multi-layer perceptron regression model, using Keras. 
+
+'''
+
+###################################
+# Module Importations (A - Z Format)
+###################################
