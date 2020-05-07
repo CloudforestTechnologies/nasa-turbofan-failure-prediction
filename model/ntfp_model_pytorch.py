f526fc207dd0b7ab6029760d2aefec877f9e4ddf
Paul Metcalf <paulmetcalf@hotmail.com>
2020-05-07 10:38:34 +0100

    Created module outline for pytorch implementation.

--- model/ntfp_model_pytorch.py
+++ model/ntfp_model_pytorch.py
@@ -9,3 +9,73 @@ This file supports training and evalaution of a Neural Network Model, using PyTo
 # Module Importations (A - Z Format)
 ###################################
 
+def create_pytorch_tensors():
+    """
+    Create and return PyTorch Tensors from input dataframe.
+    ======================================
+
+    Args:
+        data_df (dataframe) - Dataframe with engine data to be modelled.
+
+    Returns:
+        tensors (Tensors) - PyTorch Tensors for use with NN models.
+    """
+
+    pass
+
+def create_pytorch_NN():
+    """
+    Initialise and return PyTorch neural network.
+    ======================================
+
+    Args:
+        None.
+
+    Returns:
+        nn (Neural Network) - PyTorch Neural Network.
+    """
+
+    pass
+
+def train_pytorch_NN():
+    """
+    Train PyTorch neural network using Tensors.
+    ======================================
+
+    Args:
+        tensors (Tensors) - PyTorch tensor training data.
+
+    Returns:
+        nn (Neural Network) - PyTorch Neural Network.
+    """
+
+    pass
+
+def train_pytorch_NN():
+    """
+    Train PyTorch neural network using Tensors.
+    ======================================
+
+    Args:
+        tensors (Tensors) - PyTorch tensor training data.
+
+    Returns:
+        nn (Neural Network) - PyTorch Neural Network.
+    """
+
+    pass
+
+def evaluate_pytorch_NN():
+    """
+    Evaluate PyTorch neural network.
+    ======================================
+
+    Args:
+        nn (Neural Network) - PyTorch neural network to undergo evaluation.
+        test_data (Tensors) - PyTorch Tensors on which model is evaluated.
+
+    Returns:
+        evaluation_results (tuple) - Results of Neural Network evaluation.
+    """
+
+    pass
