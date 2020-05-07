e4d933e97d07355dd436d2973164fb320692cfcf
Paul Metcalf <paulmetcalf@hotmail.com>
2020-05-07 09:49:22 +0100

    Added pytorch file to Models.

---
+++ model/ntfp_model_pytorch.py
@@ -0,0 +1,11 @@
+'''
+NASA Turbofan Failure Prediction - PyTorch Neural Network Model
+
+This file supports training and evalaution of a Neural Network Model, using PyTorch and Tensors. 
+
+'''
+
+###################################
+# Module Importations (A - Z Format)
+###################################
+
