43bb1d2d3e70fb72e08122032c81201e12728f49
PMetcalf <paulmetcalf@hotmail.com>
2021-01-19 10:21:33 +0000

    Added constants file.

---
+++ Source/constants.py
@@ -0,0 +1,15 @@
+'''
+Constants
+
+Provides store of constants for use with this project
+
+'''
+
+def return_project_code():
+    return 'PM'
+
+def return_fig_save_path():
+    return r'C:\Developer\nasa-turbofan-failure-prediction\Reports\Figures'
+
+def return_model_save_path():
+    return r'C:\Developer\nasa-turbofan-failure-prediction\Models'
