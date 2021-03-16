25f84a27a58cdb999375690eeba9a0553ae978b7
Paul Metcalf <paulmetcalf@hotmail.com>
2021-03-16 16:03:14 +0000

    Constants adjustment.

--- Source/constants.py
+++ Source/constants.py
@@ -12,13 +12,13 @@ def return_project_code():
     return 'PM'
 
 def return_fig_save_path():
-    return r'D:/Developer Area/nasa-turbofan-failure-prediction/Reports/Figures'
+    return r'C:/Developer/nasa-turbofan-failure-prediction/Reports/Figures'
 
 def return_data_pickle_path():
-    return r'D:/Developer Area/nasa-turbofan-failure-prediction/Data/Interim'
+    return r'C:/Developer/nasa-turbofan-failure-prediction/Data/Interim'
 
 def return_model_save_path():
-    return r'D:/Developer Area/nasa-turbofan-failure-prediction/Models'
+    return r'C:/Developer/nasa-turbofan-failure-prediction/Models'
 
 def return_tensorboard_log_path():
 
